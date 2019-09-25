73fa0bf5ace5218a53346217fc94cf777bb78660
ALEXANDER SARSON <Alexander_Sarson@outlook.com>
2019-09-25 22:12:50 +0200

    travis test sql

--- travis.sql
+++ travis.sql
@@ -1,10 +1,9 @@
 CREATE USER 'dev'@'localhost' IDENTIFIED BY 'dev';
 GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON *.* TO 'dev'@'localhost';
-DROP DATABASE IF EXISTS QuotesDB;
-CREATE DATABASE QoutesDB;
-USE QoutesDB;
-CREATE TABLE Qoutes(
+CREATE DATABASE IF NOT EXISTS qoutesdb;
+USE qoutesdb;
+CREATE TABLE qoutes(
     qoutes VARCHAR(100) NOT NULL
 );
 
-INSERT INTO Qoutes (qoutes) VALUES ("blabla"),("klakla"),("okok"),("yes mand!"),("SUPER GODT"),("MEGET VIGTIGT");
+INSERT INTO qoutes (qoutes) VALUES ("blabla"),("klakla"),("okok"),("yes mand!"),("SUPER GODT"),("MEGET VIGTIGT");
